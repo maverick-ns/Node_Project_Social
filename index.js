dc229c76c64653e1ab5c903a1916aebeb8e338f2
maverickJAI <jaimech61@gmail.com>
2022-06-15 11:35:56 +0530

    added a controllers and router

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const port = 8000;
 
 const app = express();
 
+
+
+
+// use express router
+app.use('./', require('./routes'));
+
+
 app.listen(port,function (err) {
 if(err){
      console.log(`server:${err}`)
